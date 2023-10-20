0b3fc62dce2c8724c1ea0d5c1edb5a3f23f5c118
Gilberts Ahumada <gilbertsahumada@gmail.com>
2023-10-20 12:17:59 -0600

    Update DestinationMinter.sol

--- ccip-mint-nft/contracts/DestinationMinter.sol
+++ ccip-mint-nft/contracts/DestinationMinter.sol
@@ -1,30 +1,68 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.20;
 
-import {CCIPReceiver} from "@chainlink/contracts-ccip/src/v0.8/ccip/applications/CCIPReceiver.sol";
+import {LinkTokenInterface} from "@chainlink/contracts/src/v0.8/interfaces/LinkTokenInterface.sol";
+import {IRouterClient} from "@chainlink/contracts-ccip/src/v0.8/ccip/interfaces/IRouterClient.sol";
 import {Client} from "@chainlink/contracts-ccip/src/v0.8/ccip/libraries/Client.sol";
-import {MexicoNFT} from "./MexicoNFT.sol";
+import {Withdraw} from "./Withdraw.sol";
 
 /**
  * THIS IS AN EXAMPLE CONTRACT THAT USES HARDCODED VALUES FOR CLARITY.
  * THIS IS AN EXAMPLE CONTRACT THAT USES UN-AUDITED CODE.
  * DO NOT USE THIS CODE IN PRODUCTION.
  */
-contract DestinationMinter is CCIPReceiver {
-    MexicoNFT nft;
-
-    event MintCallSuccessfull();
-
-    constructor(address router, address nftAddress) CCIPReceiver(router) {
-        nft = MexicoNFT(nftAddress);
+contract SourceMinter is Withdraw {
+    enum PayFeesIn {
+        Native,
+        LINK
     }
 
-    function _ccipReceive(Client.Any2EVMMessage memory message)
-        internal
-        override
-    {
-        (bool success, ) = address(nft).call(message.data);
-        require(success);
-        emit MintCallSuccessfull();
+    address immutable i_router;
+    address immutable i_link;
+
+    event MessageSent(bytes32 messageId);
+
+    constructor(address router, address link) {
+        i_router = router;
+        i_link = link;
+        LinkTokenInterface(i_link).approve(i_router, type(uint256).max);
+    }
+
+    receive() external payable {}
+
+    function mint(
+        uint64 destinationChainSelector,
+        address receiver,
+        PayFeesIn payFeesIn
+    ) external {
+        Client.EVM2AnyMessage memory message = Client.EVM2AnyMessage({
+            receiver: abi.encode(receiver),
+            data: abi.encodeWithSignature("mint(address)", msg.sender),
+            tokenAmounts: new Client.EVMTokenAmount[](0),
+            extraArgs: "",
+            feeToken: payFeesIn == PayFeesIn.LINK ? i_link : address(0)
+        });
+
+        uint256 fee = IRouterClient(i_router).getFee(
+            destinationChainSelector,
+            message
+        );
+
+        bytes32 messageId;
+
+        if (payFeesIn == PayFeesIn.LINK) {
+            // LinkTokenInterface(i_link).approve(i_router, fee);
+            messageId = IRouterClient(i_router).ccipSend(
+                destinationChainSelector,
+                message
+            );
+        } else {
+            messageId = IRouterClient(i_router).ccipSend{value: fee}(
+                destinationChainSelector,
+                message
+            );
+        }
+
+        emit MessageSent(messageId);
     }
 }
